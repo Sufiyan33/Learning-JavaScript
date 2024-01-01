cef9596856ca04042d4119753ffa7ad573dba81d
Sufiyan33 <basu700736@gmail.com>
2024-01-01 20:39:32 +0530

    Exploring var, let keyword

---
+++ let.js
@@ -0,0 +1,15 @@
+console.log("let keyword :: ")
+
+var a = 12;
+let b = "sufiyan";
+var c = 109.2;
+
+var d = 'sufi'
+{
+    let b = "ahmad";
+    console.log(b)
+}
+console.log(b)
+console.log(a)
+console.log(c)
+console.log(d)
---
+++ var.js
@@ -0,0 +1,17 @@
+console.log("var keyword and it's issues :: ")
+
+var a = 12;
+var b = "sufiyan";
+var c = 109.2;
+
+var d = 'sufi'
+{
+    var b = "ahmad";
+    console.log(b)
+}
+console.log(b)
+console.log(a)
+console.log(c)
+console.log(d)
+
+// this is the issue : because var used globally hence avoid using var keyword.
