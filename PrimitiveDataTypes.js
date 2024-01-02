5efaaa4646215ab3e685e1c5bc37fb1a9128d088
Sufiyan33 <basu700736@gmail.com>
2024-01-02 15:18:08 +0530

    Exploring dataTypes

---
+++ PrimitiveDataTypes.js
@@ -0,0 +1,12 @@
+// nn bb ss u
+
+// null, number, bigInt, boolean, symbol, string, undefined
+let a = null;
+let b = 234;
+let c = BigInt(437) + BigInt(3)
+let d = true
+let e = "Sufiyan"
+let f = Symbol("I am Symbol")
+let g = undefined
+
+console.log(a,b,c,d,f,e,g)
