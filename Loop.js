75c3d7bdad944c86c80eda588eb4b4f6d1972a48
Sufiyan33 <basu700736@gmail.com>
2024-01-03 16:08:23 +0530

    Exploring for loops

---
+++ Loop.js
@@ -0,0 +1,22 @@
+// for loop
+for (let i =0; i<10; i++){
+    console.log("value of i is : " + i)
+}
+
+const obj = {
+    "Sufiyan":10,
+    "Sanu":989,
+    "Sua":109,
+    "Suina":11
+}
+
+// for in loop
+for(let a in obj){
+    console.log("Marks of " + a + " are : " + obj[a])
+}
+
+// for of loop
+for(let b of "Sufiyan"){
+    console.log(b)
+}
+
