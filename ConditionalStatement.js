67abf1d2af65143037b9c16b4edc8c95a8084eb1
Sufiyan33 <basu700736@gmail.com>
2024-01-03 12:10:15 +0530

    Exploring if -else

---
+++ ConditionalStatement.js
@@ -0,0 +1,10 @@
+let a = 20;
+console.log(a)
+
+if(a > 30){
+    console.log("Number is not greater")
+}else if(a <= 20){
+    console.log("Input number is less or equal to the number")
+}else{
+    console.log("number is very small")
+}
