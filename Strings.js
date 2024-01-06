3915bfb92ed389709b5a289ceededdf860a58eae
Sufiyan33 <basu700736@gmail.com>
2024-01-06 12:49:57 +0530

    Exploring string

---
+++ Strings.js
@@ -0,0 +1,14 @@
+console.log("Let's explore string in JavaScript")
+
+let friends = "Sufiyan"
+let str = 'Ahmad'
+console.log(friends, str)
+
+// string using template literal
+
+let strr = `my first name is ${friends} and last name is ${str}`
+console.log(strr)
+
+let ss = 'My name is \n sufiyan'
+console.log(ss)
+
