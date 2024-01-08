14a3593d14336faa6bad4363537b2b55d92be243
Sufiyan33 <basu700736@gmail.com>
2024-01-08 10:14:03 +0530

    Arrays

---
+++ Array.js
@@ -0,0 +1,9 @@
+let marks=[12,2,3,44,64,645,34, null, false,"not given"]
+console.log(marks)
+
+for(let i=0; i< marks.length; i++){
+    console.log("Arrays number are : "+ marks[i])
+}
+
+// when the element is not given in array and want to print then it will print undefined.
+console.log(marks[12])
