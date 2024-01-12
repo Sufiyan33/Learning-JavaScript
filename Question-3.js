d1806930b9683b14ab6cf5757fbc5bf2b0448c5f
Sufiyan33 <basu700736@gmail.com>
2024-01-12 10:01:19 +0530

    Adding numbers in array

---
+++ Question-3.js
@@ -0,0 +1,7 @@
+//Take input from user and add in array.
+
+let arr = [12,1,2,3,35,32,232,55]
+let input = prompt("Enter a number ")
+input = Number.parseInt(input)
+arr.push(input)
+console.log(arr)
