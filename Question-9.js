38c3dc4731b601e3c7e2b9c6460c290246fa3724
Sufiyan33 <basu700736@gmail.com>
2024-02-21 12:27:57 +0530

    Change all li tag color to red or cyan

--- Question-9.js
+++ Question-9.js
@@ -2,3 +2,10 @@ document.getElementsByTagName("nav")[0].firstElementChild.style.color="red"
 
 document.getElementsByTagName("nav")[0].firstElementChild.style.color="green"
 document.getElementsByTagName("nav")[0].lastElementChild.style.color="green"
+
+// chage all li tag color to cyan
+// Appraoch - first make array then use forEach loop
+
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background = "cyan";
+})
