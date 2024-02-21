93be33cf11a20848f65affc4595cc355666231b5
Sufiyan33 <basu700736@gmail.com>
2024-02-21 15:07:20 +0530

    Exploring innerHTML, outerHTML and many more child

---
+++ EventsAndDOM.js
@@ -0,0 +1,14 @@
+// check value in consloe that what it is printing...
+console.log(document.body.firstChild.nodeName)
+console.log(document.body.firstElementChild.nodeName)
+
+let x = document.getElementsByTagName('span')[0]
+console.log(x)
+let y = document.getElementsByTagName('span')[0]
+console.log(y)
+
+
+console.log(document.body.textContent)
+console.log(document.body.firstChild)
+console.log(document.body.firstChild.data)
+console.log(document.body.firstChild.nodeValue)
