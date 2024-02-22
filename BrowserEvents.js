07aa2f00050365d805a25db8ae13fbba3ac82d7a
Sufiyan33 <basu700736@gmail.com>
2024-02-22 17:56:59 +0530

    Exploring browser events

--- BrowserEvents.js
+++ BrowserEvents.js
@@ -3,3 +3,22 @@ a.onclick=()=>{
     let b = document.getElementsByClassName("container")[0]
     b.innerHTML="Hello world"
 }
+
+let btn = document.getElementById("btn")
+let x = function(e) {
+    alert("Hello India")
+}
+
+let y = function(e){
+    alert("Hello Bharat")
+}
+
+btn.addEventListener('click', x)
+btn.addEventListener('click', y)
+
+let p = prompt("what is your favourite country?")
+if(p == "1"){
+    btn.removeEventListener('click', y)
+}else if(p == "2"){
+    btn.removeEventListener('click', x)
+}
