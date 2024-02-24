8f606aefec341ba42ffc9fbad98552fd2582d021
Sufiyan33 <basu700736@gmail.com>
2024-02-24 09:53:56 +0530

    Adding eventListener in buttons

--- Question-10.js
+++ Question-10.js
@@ -1,1 +1,16 @@
 console.log("Write program to show different alert when different button has clicked...")
+
+document.getElementById("google").addEventListener("click", function() {
+    window.location="https://google.com";
+    window.focus();
+})
+
+document.getElementById("fb").addEventListener("click", function() {
+    window.location="https://fb.com";
+    window.focus();
+})
+
+document.getElementById("x").addEventListener("click", function() {
+    window.location="https://twitter.com";
+    window.focus();
+})
