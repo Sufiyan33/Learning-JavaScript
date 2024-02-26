a376a81486e83e33c19e30673de2450e2a332059
Sufiyan33 <basu700736@gmail.com>
2024-02-26 12:29:36 +0530

    Handling error

--- CallBacks.js
+++ CallBacks.js
@@ -21,17 +21,32 @@ function loadScript(src, callback){
     script.src=src
     script.onload = function() {
         console.log("Script has been loaded : " + src)
-        callback(src);
+        callback(null,src); // you can also pass parameters in callBack.
+    }
+    // Let's handle error if script is not loaded
+    script.onerror= function(){
+        console.log("Error loaded with script : " + src)
+        callback(new Error("Script got some error"))
     }
     document.body.appendChild(script)
 }
 
-function hello(src){
-    alert("Hello world!!!" + src) 
+function hello(error,src){
+    if(error){
+        console.log(error)
+        return
+    }else{
+        alert("Hello world!!!" + src) 
+    }
 }
 
-function goodMorning(src){
+function goodMorning(error, src){
+    if(error){
+        console.log(error)
+    }
     alert("Good morning" + src)
 }
 
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", goodMorning)
+loadScript("https://cdn.jssdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", hello)
+
+// May be somethime script will not load then in this case we need to handle Exception let's handle...
