e1701598545c7286c8d35f96c276763569a0eaa5
Sufiyan33 <basu700736@gmail.com>
2024-02-29 23:59:00 +0530

    Handling various errors

--- CustomeError.js
+++ CustomeError.js
@@ -11,10 +11,19 @@
 try{
     //throw new Error("Something occured...")
     // Here you can also use ReferenceError...
-
-    throw new ReferenceError("Something occuered ...")
+    let age = prompt("Enter your age...")
+    let b = Number.parseInt(age);
+    if(age < 18){
+        throw new ReferenceError("You are not 18+ ...")
+    }
 }catch(error){
     console.log(error.name)
     console.log(error.message)
     console.log(error.stack)
 }
+
+console.log("Script is still running...")
+
+/*
+    if you want to explore more then search in google "mdn reference js errors" and navigate the page...
+*/
