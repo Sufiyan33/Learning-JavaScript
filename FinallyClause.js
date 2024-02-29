c19276783747c652ffa1381aa00af50b9947b6c6
Sufiyan33 <basu700736@gmail.com>
2024-03-01 00:17:08 +0530

    Handling finallu

---
+++ FinallyClause.js
@@ -0,0 +1,15 @@
+try{
+    let a = 0;
+    console.log(sufiyan)
+    console.log("Program ran successfully")
+}catch(error){
+    console.log("Some error occured...")
+    //Lets throw error in catch. so that you can understand purpose of finally
+    console.log(Ahmad)
+}finally{
+    console.log("I am finally & will always execute...")
+
+    // close the file.
+    // exit loop.
+    //write to the log file...
+}
