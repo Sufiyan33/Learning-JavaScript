fda4035607b1d1c39c2fd5567143ab2b48553a49
Sufiyan33 <basu700736@gmail.com>
2024-02-29 15:06:50 +0530

    Exploring promise allSetteled method

--- PromiseApi.js
+++ PromiseApi.js
@@ -16,7 +16,7 @@ let p3 = new Promise((resolve, reject) =>{
     }, 3000)
 })
 
-p1.then((value)=>{
+/*p1.then((value)=>{
     console.log(value)
 })
 
@@ -28,7 +28,7 @@ p2.then((value)=>{
 
 p3.then((value)=>{
     console.log(value)
-})
+})*/
 
 /*
     in this case all promises will resolve after time interval but if you want to get all promise to be resolve or display at a time.
@@ -36,8 +36,31 @@ p3.then((value)=>{
     For example real time fetching something from network and want to display at once then use promise api...
 */
 
-console.log("---- Fetching using prmise.All method -----")
 let promie_all = Promise.all([p1, p2, p3])
 promie_all.then((value) =>{
+    console.log(value)
+})
+
+/*
+    if any promises failed or rejected then prmoise.all method will return error. Although 1 rejected and  2 resolve but it will
+    give you error.
+    In order to resolve this issue, we will use promise.allSettled method.
+    Let first find issues
+ */
+
+let p4 = new Promise((resolve, reject) =>{
+    setTimeout(()=>{
+        reject(new Error("Something missing..."));
+    }, 4000)
+})
+
+/*let promises_all = Promise.all([p1, p2, p3, p4])
+promises_all.then((value)=>{
+    console.log(value)
+})*/
+
+// to resolve above issue i will use promise.AllSettled method
+let promis_all = Promise.allSettled([p1,p2,p3,p4])
+promis_all.then((value)=>{
     console.log(value)
 })
