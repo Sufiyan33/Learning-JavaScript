8074c50a97569d8db551ccb5d482508a7d3916f9
Sufiyan33 <basu700736@gmail.com>
2024-02-29 23:50:07 +0530

    Handling custome exception

--- CustomeError.js
+++ CustomeError.js
@@ -1,5 +1,20 @@
-try{
+/*try{
     console.log(rahul)
 }catch(error){
     console.log(error.name)
+}*/
+
+/*
+    let's throw custome exception here.
+*/
+
+try{
+    //throw new Error("Something occured...")
+    // Here you can also use ReferenceError...
+
+    throw new ReferenceError("Something occuered ...")
+}catch(error){
+    console.log(error.name)
+    console.log(error.message)
+    console.log(error.stack)
 }
