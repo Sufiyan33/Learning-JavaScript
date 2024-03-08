6e0e6de1577c2fc8cecb17696f955e8b468145dd
Sufiyan33 <basu700736@gmail.com>
2024-03-08 11:07:01 +0530

    different ways of this

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -28,8 +28,19 @@ const x = {
     exp: 10,
 
     show: function(){
-        console.log(`The name is ${this.name} \nThe role is ${this.role} \nAnd experience is ${this.exp}`) // here this refers to object x...
+        //console.log(`The name is ${this.name} \nThe role is ${this.role} \nAnd experience is ${this.exp}`) // here this refers to object x...
+        setTimeout(function() {
+            console.log(this)
+            console.log(`The name is ${this.name} \nThe role is ${this.role} \nAnd experience is ${this.exp}`)
+        }, 2000);
     }
 }
 //console.log(x.name, x.role, x.exp)
 x.show()
+
+/*
+    There are 3 different value of this :
+    1 : if you print this alone or outside everything then it will return Global object.
+    2 : if you print inside function which use function keyword like above then this refer to window object.
+    3 : if you print this inside of any event then this refer to event elements.
+*/
