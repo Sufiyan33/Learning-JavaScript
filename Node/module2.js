9fca64cfa994eceebae571aad7f891f362cfd81a
Sufiyan33 <basu700736@gmail.com>
2024-03-09 21:29:49 +0530

    using destructuring to access object directly

--- Node/module2.js
+++ Node/module2.js
@@ -1,12 +1,27 @@
 /*
     To import the module1 you can use below expression...
 */
-const hello = require("./module1")
+//const hello = require("./module1")
 //hello() //calling hello function...
 
 // if you have multiple module then use below expression : 
-hello.hello()
+/*hello.hello()
 hello.advanceHello("Sufiyan")
 hello.advanceHello("Ahmad")
 hello.advanceHello("Sabir")
-hello.advanceHello("Alfaz")
+hello.advanceHello("Alfaz") */
+
+/*
+    Problem :: see here, each and every time you need hello function to call another function.. that is little bit annoying.
+    Solution :: You can use destructuring for directly accessing object. 
+    TODO :: todo that I am commenting above line no 4.
+*/
+
+console.log("--- Using desctructuring ---")
+
+const {hello, advanceHello} = require("./module1")
+hello()
+advanceHello("Sufiyan")
+advanceHello("Ahmad")
+advanceHello("Sabir")
+
