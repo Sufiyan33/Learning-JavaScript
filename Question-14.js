18d33e276f079daf910f7f444a29ec4837d4cf0b
Sufiyan33 <basu700736@gmail.com>
2024-05-13 13:56:24 +0530

    Calculating bill

---
+++ Question-14.js
@@ -0,0 +1,7 @@
+/*
+    A person needs a simple tip calculator for restaurent. If bill value is between 50 and 300 then tip is 15% otherwise 20%.
+*/
+
+const bill = 275;
+const tip = bill <=300 && bill >=50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${bill + tip}.`);
