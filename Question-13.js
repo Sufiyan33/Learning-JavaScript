8b172ba4de7657701e0109b9a8aafcde0aa81276
Sufiyan33 <basu700736@gmail.com>
2024-05-13 13:03:30 +0530

    Calculating mass of john and mark

---
+++ Question-13.js
@@ -0,0 +1,9 @@
+const massMark = 98;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// ** used for exponential or expo operator
+const BMIMark = massMark / (heightMark ** 2);
+const BMIJohn = massJohn / (heightJohn ** 2);
+console.log(BMIJohn, BMIMark);
