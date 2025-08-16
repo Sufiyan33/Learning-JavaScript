d0410d6e4d276f4fb96aaaa452134c60c2af5179
alfazraja1 <arazaaansari@gmail.com>
2025-08-16 13:22:37 +0530

    Testing

--- Question-14.js
+++ Question-14.js
@@ -5,3 +5,6 @@
 const bill = 275;
 const tip = bill <=300 && bill >=50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${bill + tip}.`);
+/*
+    Adding this line for testing github actions.
+*/
